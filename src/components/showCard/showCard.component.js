4d291a5e7ff47676065a4be2022d80366a860dbd
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-29 12:32:52 -0300

    add genre-pills to the show-card component

--- src/components/showCard/showCard.component.js
+++ src/components/showCard/showCard.component.js
@@ -1,13 +1,14 @@
 const React = require('react');
+const GenresPills = require('../GenresPills/genresPills.component');
+//<img className="show-card__image" src={props.image.original} />
 
 const ShowCard = props => {
     return (
         <section>
             <div className="show-card__info">
-                <p>{props.name}</p>
-                <GenresPills genres={props.genres} />
-            </div>
-            <img className="show-card__image" src={props.image.medium} />
+                <p>{props.show.name}</p>
+                <GenresPills genres={props.show.genres} />
+            </div>    
         </section>
     );
 };
