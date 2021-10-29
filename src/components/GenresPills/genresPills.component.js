1588d069388c24135aaaa5a71cc38023f1a1df61
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-29 12:32:17 -0300

    create gender-pills component

---
+++ src/components/GenresPills/genresPills.component.js
@@ -0,0 +1,17 @@
+const React = require('react');
+
+const GenresPills = props => {
+    if (props.genres.length === 0) {
+        return <div></div>;
+    }
+
+    return (
+        <div className="genres-pills">
+            {props.genres.map(genre => {
+                return <span data-testid="genre-pill">{genre}</span>
+            })}
+        </div>
+    );
+};
+
+module.exports = GenresPills;
