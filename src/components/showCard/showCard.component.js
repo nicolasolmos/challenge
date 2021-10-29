50a3b521d0a78510cffe145c7aac5a9b9f47bbb8
Nicolas Olmos <desa.nolmos@gmail.com>
2021-10-29 15:58:11 -0300

    styling

--- src/components/showCard/showCard.component.js
+++ src/components/showCard/showCard.component.js
@@ -11,10 +11,15 @@ const ShowCard = props => {
         imageElement =  <img className="show-card__image" src={props.show.image.original} />;
     }
 
+    const dangerousHTMLInjection = {
+        __html: props.show.summary,
+    };
+
     return (
         <section className="show-card">
             <div className="show-card__info">
                 <p className="show-card__name">{props.show.name}</p>
+                <span dangerouslySetInnerHTML={dangerousHTMLInjection} />
                 <GenresPills genres={props.show.genres} />
             </div>    
             {imageElement}
